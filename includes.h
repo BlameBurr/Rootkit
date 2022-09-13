63b9e16741cd35577e1e3091a38abc333874d45a
Burr <33370725+BlameBurr@users.noreply.github.com>
2022-09-13 16:03:47 +0100

    Update includes.h

--- includes.h
+++ includes.h
@@ -5,10 +5,6 @@
 #include <linux/syscalls.h>
 #include <linux/namei.h>
 #include <linux/syscalls.h>
-#include <linux/ftrace.h>
-#include <linux/linkage.h>
-#include <linux/slab.h>
-#include <linux/uaccess.h>
 
 #if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
 #define PTREGS_SYSCALL_STUBS 1
