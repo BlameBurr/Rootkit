355b6ae17aaedc6b3f00e6e232c97e701b16ff48
Burr <33370725+BlameBurr@users.noreply.github.com>
2022-09-13 19:19:09 +0100

    Update ftrace_helper.h

--- ftrace_helper.h
+++ ftrace_helper.h
@@ -5,29 +5,29 @@
     * License: GPL
 */
 
-//Includes
 #include <linux/ftrace.h>
 #include <linux/linkage.h>
 #include <linux/slab.h>
 #include <linux/uaccess.h>
-#include "common.h"
 
-//Some kernel version shims
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,7,0))
-#include <linux/kprobes.h>
+#if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
+#define PTREGS_SYSCALL_STUBS 1
+#endif
+
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,11,0))
+#define FTRACE_REGS_REC 1
+#define FTRACE_OPS_FL_RECURSION_SAFE FTRACE_OPS_FL_RECURSION
+#endif
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5,7,0)
 #define KPROBE_LOOKUP 1
+#include <linux/kprobes.h>
 static struct kprobe kp = {
     .symbol_name = "kallsyms_lookup_name"
 };
 #endif
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,11,0))
-#define FTRACE_REGS_REC 1
-#endif
-
 //Main functions and definitions
-#define USE_FENTRY_OFFSET 0 //Prevent recursive loop by detection of return address (0) or by jumping over ftrace call (1)
-
 #ifdef PTREGS_SYSCALL_STUBS
 #define SYSCALL_NAME(name) ("__x64_" name)
 #else
@@ -35,26 +35,19 @@ static struct kprobe kp = {
 #endif
 
 #define HOOK(_name, _hook, _orig)   \
-{                                   \
-    .name = SYSCALL_NAME(_name),    \
-    .function = (_hook),            \
-    .original = (_orig),            \
+{                   \
+    .name = SYSCALL_NAME(_name),        \
+    .function = (_hook),        \
+    .original = (_orig),        \
 }
+
+#define USE_FENTRY_OFFSET 0 //Prevent recursive loop by detection of return address (0) or by jumping over ftrace call (1)
 
 #if !USE_FENTRY_OFFSET
 #pragma GCC optimize("-fno-optimize-sibling-calls")
 #define ADDRESS(addr) (addr);
 #else
 #define ADDRESS(addr) (addr+MCOUNT_ISN_SIZE)
-#endif
-
-#ifdef FTRACE_REGS_REC
-#define FTRACE_OPS_FL_RECURSION_SAFE FTRACE_OPS_FL_RECURSION
-#else
-#define ftrace_regs pt_regs
-static __always_inline struct pt_regs *ftrace_get_regs(struct ftrace_regs *regs) {
-    return regs;
-}
 #endif
 
 struct ftrace_hook {
@@ -74,6 +67,7 @@ static int fh_resolve_hook_address(struct ftrace_hook *hook) {
     unregister_kprobe(&kp);
     #endif
     hook->address = kallsyms_lookup_name(hook->name);
+    printk(KERN_DEBUG "%s address is %p\n", hook->name, hook->address);
     if (!hook->address) {
         printk(KERN_DEBUG "fh_resolve_hook_address failed to resolve symbol: %s\n", hook->name);
         return -ENOENT;
@@ -83,14 +77,21 @@ static int fh_resolve_hook_address(struct ftrace_hook *hook) {
     return 0;
 };
 
+#if !FTRACE_REGS_REC
+#define ftrace_regs pt_regs
+static __always_inline struct pt_regs *ftrace_get_regs(struct ftrace_regs *fregs)
+{
+	return fregs;
+}
+#endif
 static void notrace fh_ftrace_thunk(unsigned long ip, unsigned long parent_ip, struct ftrace_ops *ops, struct ftrace_regs *fregs) {
     struct pt_regs *regs = ftrace_get_regs(fregs);
     struct ftrace_hook *hook = container_of(ops, struct ftrace_hook, ops);
-    #if !USE_FENTRY_OFFSET
+    #if USE_FENTRY_OFFSET
+    regs->ip = (unsigned long) hook->function;
+    #else
     if (!within_module(parent_ip, THIS_MODULE))
         regs->ip = (unsigned long) hook->function;
-    #else
-    regs->ip = (unsigned long) hook->function;
     #endif
 };
 
