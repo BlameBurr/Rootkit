28a68dba0cfb1f91b1c85a5524e18a72a6f4f6d8
Burr <33370725+BlameBurr@users.noreply.github.com>
2022-09-14 01:05:20 +0100

    Update common.h

--- common.h
+++ common.h
@@ -5,6 +5,7 @@
 #include <linux/syscalls.h>
 #include <linux/namei.h>
 
+#define CONFIG_x86_64 1
 #if defined(CONFIG_x86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
 #define PTREGS_SYSCALL_STUBS 1
 #endif
