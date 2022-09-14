ef19ad8335d72886870aa1616f1b9d7c5e6c5726
Burr <33370725+BlameBurr@users.noreply.github.com>
2022-09-14 01:28:16 +0100

    Update common.h

--- common.h
+++ common.h
@@ -1,9 +1,18 @@
 #include <linux/init.h>
+#include <linux/ftrace.h>
+#include <linux/linkage.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/version.h>
 #include <linux/syscalls.h>
 #include <linux/namei.h>
+#include <linux/delay.h>
+#include <linux/kthread.h>
+#include <asm/signal.h>
+#include <linux/sched.h>
+#include <linux/sched/signal.h>
 
 #if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
 #define PTREGS_SYSCALL_STUBS 1
