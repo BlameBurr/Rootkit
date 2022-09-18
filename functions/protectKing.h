f662b18cab5771cfd9d7043b0fe47050e8ad4526
Burr <33370725+BlameBurr@users.noreply.github.com>
2022-09-18 19:49:26 +0100

    Update protectKing.h

--- functions/protectKing.h
+++ functions/protectKing.h
@@ -1,13 +1,13 @@
 #include "../common.h"
 
 #ifdef PTREGS_SYSCALL_STUBS
-static asmlinkage long (*ogWrite)(struct pt_regs *regs);
+extern asmlinkage long (*ogWrite)(struct pt_regs *regs);
 asmlinkage long hookWrite(struct pt_regs *regs);
-static asmlinkage long (*ogOpenAt)(struct pt_regs *regs);
+extern asmlinkage long (*ogOpenAt)(struct pt_regs *regs);
 asmlinkage long hookOpenAt(struct pt_regs *regs);
 #else
-static asmlinkage long (*ogWrite)(unsigned int fd, const char __user *buf, size_t count);
+extern asmlinkage long (*ogWrite)(unsigned int fd, const char __user *buf, size_t count);
 asmlinkage long hookWrite(unsigned int fd, const char __user *buf, size_t count);
-static asmlinkage long (*ogOpenAt)(int dfd, const char __user *filename, int flags, umode_t mode);
+extern asmlinkage long (*ogOpenAt)(int dfd, const char __user *filename, int flags, umode_t mode);
 asmlinkage long hookOpenAt(int dfd, const char __user *filename, int flags, umode_t mode);
 #endif
