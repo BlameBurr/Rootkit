01a2351e143b5368086c7aaab7113c39f9b743e2
Burr <33370725+BlameBurr@users.noreply.github.com>
2022-09-18 19:49:14 +0100

    Update protectKing.c

--- functions/protectKing.c
+++ functions/protectKing.c
@@ -3,15 +3,17 @@
 unsigned int targetFD = 0;
 
 #ifdef PTREGS_SYSCALL_STUBS
+asmlinkage long (*ogWrite)(struct pt_regs *regs);
 asmlinkage long hookWrite(struct pt_regs *regs) {
 	if (regs->di == targetFD) {
 		printk(KERN_INFO, "PID: %d tried to write to target file.\n", current->pid);
-		return 0;
+		return EBADF;
 	}
 
 	return ogWrite(regs);
 }
 #else
+asmlinkage long (*ogWrite)(unsigned int fd, const char __user *buf, size_t count);
 asmlinkage long hookWrite(unsigned int fd, const char __user *buf, size_t count) {
 	if (fd == targetFD) {
 		printk(KERN_INFO, "PID: %d tried to write to target file.\n", current->pid);
@@ -22,6 +24,7 @@ asmlinkage long hookWrite(unsigned int fd, const char __user *buf, size_t count)
 }
 #endif
 #ifdef PTREGS_SYSCALL_STUBS
+asmlinkage long (*ogOpenAt)(struct pt_regs *regs);
 asmlinkage long hookOpenAt(struct pt_regs *regs) {
 	char *buf = kzalloc(4096, GFP_KERNEL);
 	
@@ -44,6 +47,7 @@ asmlinkage long hookOpenAt(struct pt_regs *regs) {
 	return ogOpenAt(regs);
 }
 #else
+asmlinkage long (*ogOpenAt)(int dfd, const char __user *filename, int flags, umode_t mode);
 asmlinkage long hookOpenAt(int dfd, const char __user *filename, int flags, umode_t mode){
 	char *buf = kzalloc(4096, GFP_KERNEL);
 	
