df018bdac7e5b64ba6e58acfb0cd984e76f20c1f
Burr <33370725+BlameBurr@users.noreply.github.com>
2022-09-18 22:15:43 +0100

    Update and rename protectKing.h to openAt.h

--- functions/openAt.h
+++ functions/openAt.h
@@ -1,13 +1,9 @@
 #include "../common.h"
 
 #ifdef PTREGS_SYSCALL_STUBS
-extern asmlinkage long (*ogWrite)(struct pt_regs *regs);
-asmlinkage long hookWrite(struct pt_regs *regs);
 extern asmlinkage long (*ogOpenAt)(struct pt_regs *regs);
 asmlinkage long hookOpenAt(struct pt_regs *regs);
 #else
-extern asmlinkage long (*ogWrite)(unsigned int fd, const char __user *buf, size_t count);
-asmlinkage long hookWrite(unsigned int fd, const char __user *buf, size_t count);
 extern asmlinkage long (*ogOpenAt)(int dfd, const char __user *filename, int flags, umode_t mode);
 asmlinkage long hookOpenAt(int dfd, const char __user *filename, int flags, umode_t mode);
 #endif
