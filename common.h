3013f1d24cb7f44d950273fadd21320c9df93ff2
Burr <33370725+BlameBurr@users.noreply.github.com>
2022-10-18 23:00:24 +0100

    Update common.h
    Added dependency

--- common.h
+++ common.h
@@ -12,6 +12,7 @@
 #include <linux/kthread.h>
 #include <asm/signal.h>
 #include <linux/sched.h>
+#include <linux/fdtable.h>
 #include <linux/sched/signal.h>
 
 #if defined(CONFIG_X86_64) && (LINUX_VERSION_CODE >= KERNEL_VERSION(4,17,0))
