8f77d3940f563b989071a28a55f4c8524f7297af
Burr <33370725+BlameBurr@users.noreply.github.com>
2022-11-01 01:39:21 +0000

    Update openAt.c
    Potential changes, not currently working. Closes terminal, causes dmesg to hang and requires os restart.

--- functions/openAt.c
+++ functions/openAt.c
@@ -1,19 +1,44 @@
 #include "openAt.h"
-
-short closeProcess = 1;
+short closeProcess = 0;
 
 #ifdef PTREGS_SYSCALL_STUBS
+char *getcwd(struct task_struct *task) {
+	char *cwd, *buf;
+    struct path pwd, root; 
+	
+	pwd = task->fs->pwd;
+    path_get(&pwd);
+    root=  task->fs->root;
+    path_get(&root);
+    buf = kzalloc(4096, GFP_KERNEL);
+    cwd = d_path(&pwd,buf, sizeof(buf));
+	kfree(buf);
+	return cwd;
+}
+
 asmlinkage long (*ogOpenAt)(struct pt_regs *regs);
 asmlinkage long hookOpenAt(struct pt_regs *regs) {
-	char *buf = kzalloc(4096, GFP_KERNEL);
+	char *buffer = kzalloc(4096, GFP_KERNEL);
+	char *filename = kzalloc(4096, GFP_KERNEL);
+	char *dir = kzalloc(4096, GFP_KERNEL);
 	long ret = 0;
 	int flags = regs->dx;
-	if (!buf) return ret;
+	int dfd = regs->di;
 
-	if ((strncpy_from_user(buf, (char *)regs->si, 4096) > 0) && (strncmp(buf, "/tmp/test.txt", 13) == 0))
-		if ((flags & O_WRONLY) == O_WRONLY || (flags & O_RDWR) == O_RDWR || (flags & O_APPEND) == O_APPEND) ret = -1;
+	if (!buffer || !filename) return ret;
 
-	kfree(buf);
+	if (strncpy_from_user(buffer, (char *)regs->si, 4096) > 0) {
+		strcpy(filename, buffer);
+		strcpy(dir, getcwd(current));
+		if (strstr(buffer, "test.txt") != 0) {
+			printk(KERN_INFO "DFD: %d, Filename: %s, Dir: %s\n", dfd, filename, dir);
+			if ((flags & O_WRONLY) == O_WRONLY || (flags & O_RDWR) == O_RDWR || (flags & O_APPEND) == O_APPEND) ret = -1;
+		}
+	}
+
+	kfree(buffer);
+	kfree(filename);
+	kfree(dir);
 	
 	if ((ret == -1) && (closeProcess == 1)) {
 		struct kernel_siginfo info;
